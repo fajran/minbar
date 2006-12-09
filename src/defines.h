2466c1802cb62f18722ddeaa75ac75148d0fcd54
djihed <djihed@b7dd3e64-9a96-433d-bc4c-c23e47d23892>
2006-12-09 03:40:46 +0000

    gprayer.glade gets pixmaps by #define'ed vars.
    git-svn-id: https://svn.arabeyes.org/svn/projects/trunk/itl/programs/minbar@2021 b7dd3e64-9a96-433d-bc4c-c23e47d23892

--- src/defines.h
+++ src/defines.h
@@ -4,6 +4,7 @@
 /* Pictures, Icons, Graphics, etc */
 #define GPRAYER_KAABA_ICON	"kaabatrayicon.png"
 #define GPRAYER_BIG_LOGO	"gprayerlogo.png"
+#define GPRAYER_QURAN		"quran.png"
 
 /* Markup for time label */ 
 #define MARKUP_NORMAL_START	"<span color='blue'><b>"
